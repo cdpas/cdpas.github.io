3dea713495b03a499e2fe6e3b3be63e64cf39133
cdpas <cdpas@protonmail.com>
2024-10-19 18:17:16 +0300

    style update

--- index.js
+++ index.js
@@ -14,10 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
                 const deltaY = mouseY - eyeCenterY;
                 const angle = Math.atan2(deltaY, deltaX);
 
-                const distance = Math.min(Math.hypot(deltaX, deltaY), 10); // Maximum movement
+                const distance = Math.min(Math.hypot(deltaX, deltaY), 10); 
 
                 eye.style.transform = `translate(${Math.cos(angle) * distance}px, ${Math.sin(angle) * distance}px)`;
             });
         });
+
+          console.log(
+            '%cPlease visit',
+            'text-align: center;font-size: 20px; color: #fff; background-color: #333; padding: 10px; border-radius: 5px; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7), 4px 4px 8px rgba(0, 0, 0, 0.6), 6px 6px 12px rgba(0, 0, 0, 0.5);',
+            'https://cdpas.dev'
+          );
+
 });
 
